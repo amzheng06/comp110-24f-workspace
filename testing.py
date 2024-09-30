540fd13d319f5b61344138040ea535a53fe8f4d0
amzheng06 <amzheng@unc.edu>
2024-09-30 17:46:38 -0400

    updates to testing

--- testing.py
+++ testing.py
@@ -6,16 +6,30 @@ __author__: str = "730776315"
 # cl11 - global variables and scope
 
 
-def remove_chars(msg: str, char: str) -> str:
-    """return copy of msg w/o char"""
-    copy: str = ""
-    index: int = 0
-    while index < len(msg):
-        if msg[index] != char:
-            copy += msg[index]
-        index += 1
-    return copy
+# def remove_chars(msg: str, char: str) -> str:
+#    """return copy of msg w/o char"""
+#    copy: str = ""
+#    index: int = 0
+#    while index < len(msg):
+#        if msg[index] != char:
+#            copy += msg[index]
+#        index += 1
+#    return copy
 
 
-word: str = "yoyo"
-print(remove_chars(word, "o"))
+# word: str = "yoyo"
+# print(remove_chars(word, "o"))
+
+
+# cl13 - lists
+
+my_number: list[float] = []
+
+grocer_list: list[str] = []
+
+grocer_list.append("bananas")
+grocer_list.append("bananas")
+grocer_list.append("bananas")
+grocer_list[2] = "eggs"
+
+print(grocer_list)
