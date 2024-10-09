cad76f6bcd0881787f93adc9db5cfcaa2e5422cd
amzheng06 <amzheng@unc.edu>
2024-10-09 12:36:51 -0400

    completed cq06

--- testing.py
+++ testing.py
@@ -23,13 +23,26 @@ __author__: str = "730776315"
 
 # cl13 - lists
 
-my_number: list[float] = []
+# my_number: list[float] = []
 
-grocer_list: list[str] = []
+# grocer_list: list[str] = []
 
-grocer_list.append("bananas")
-grocer_list.append("bananas")
-grocer_list.append("bananas")
-grocer_list[2] = "eggs"
+# grocer_list.append("bananas")
+# grocer_list.append("bananas")
+# grocer_list.append("bananas")
+# grocer_list[2] = "eggs"
 
-print(grocer_list)
+# print(grocer_list)
+
+
+# cl15 - for loops
+
+names: list[str] = ["Alyssa", "Janet", "Vrinda"]
+
+for idx in range(0, len(names)):  # index for loop method
+    print(f"{idx}: {names[idx]}")
+
+index: int = 0
+while index < len(names):
+    print(f"{index}: {names[index]}")
+    index += 1
